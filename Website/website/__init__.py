d741941f9f336d473a6a1c1daa0ad529f88735f3
Johannes Heidecke <johannes.heidecke@city.ac.uk>
2021-03-31 21:00:26 +0100

    bug fixes 3

--- Website/website/__init__.py
+++ Website/website/__init__.py
@@ -3,11 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+# Define flask architecture
 app = Flask(__name__)
+# Define secret key
 app.config['SECRET_KEY'] = 'ec05b867d65fdf756ee9e26be38b9381'
+# Define database architecture - SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# Define database object
 db = SQLAlchemy(app)
+# Define bycrypt object
 bcrypt = Bcrypt(app)
+# Define loginmanager - manages logines. 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
--- Website/website/routes.py
+++ Website/website/routes.py
@@ -1,3 +1,4 @@
+# Import Libraries and other modules from website directory
 import os
 import secrets
 import pandas as pd
@@ -10,57 +11,89 @@ from flask_login import login_user, current_user, logout_user, login_required
 from website.run import handler
 import _pickle as pk
 
+
+# Define Home Page
 @app.route("/")
 @app.route("/home")
 def home():
 	return render_template('index.html')
 
+# Define About Page
 @app.route("/about")
 def about():
 	return render_template('about.html', title='About')
 
+# Define Register Page - includes POST and GET requests
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+	# If the current user is authenticated
+	# they are redircted to the account page.
 	if current_user.is_authenticated:
 		return redirect(url_for('account'))
 
+	# Get registration form
 	form = RegistrationForm()
+	# Check if form input is valid
 	if form.validate_on_submit():
+		# Hash the password before saving it
 		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		# Save user data
 		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		# Commit data to database
 		db.session.add(user)
 		db.session.commit()
+		# 'Flash' success message to user, and redirect to account page.
 		flash(f'Your account has been created {form.username.data}. You are able to login now.', 'success')
 		return redirect(url_for('account'))
 	return render_template('register.html', title='Register', form=form)
 
+# Define Login page
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+	# If the current user is authenticated
+	# they are redircted to the account page.
 	if current_user.is_authenticated:
 		return redirect(url_for('account'))
 
+	# Get login form
 	form = LoginForm()
+	# Check if form input is valid
 	if form.validate_on_submit():
+		# Define user email
 		user = User.query.filter_by(email=form.email.data).first()
+		# Check if password and email are correct
 		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			# Login user
 			login_user(user, remember=form.remember.data)
+			# If the user tried to access pages that are 
+			# inaccessable to unregistered users, the user is redirected
+			# to the page he tried to access.
 			next_page = request.args.get('next')
+			# Otherwise redirect to account page
 			return redirect(next_page) if next_page else redirect(url_for('account'))
 		else:
+			# If login failed flash 'fail' message
 			flash('Login Unsuccessful. Please check you details', 'danger')
 	return render_template('login.html', title='Login', form=form)
 
+# Define logout page
 @app.route("/logout")
 def logout():
+	# The user is logged out, and 
+	# redirected to home page.
 	logout_user()
 	return redirect(url_for('home'))
 
+# Define account page - requires login
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+	# Get update form
     form = UpdateAccountForm()
+    # Check if input is valid
     if form.validate_on_submit():
         if form.picture.data:
+        	# Saves picture, username, and email changes
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
         current_user.username = form.username.data
@@ -69,29 +102,35 @@ def account():
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
     elif request.method == 'GET':
+    	# Get user username and email
         form.username.data = current_user.username
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
 
+# Define query page - requires login
 @app.route("/query", methods=['GET', 'POST'])
 @login_required
 def query():
+	# Get input form
 	form = DataInputForm()
+	# Check if input is valid
 	if form.validate_on_submit():
 		if form.csv.data:
-			csv_file, csv_name, ''' user_path''', data_frame, recover_title = save_csv(form.csv.data, current_user.id)
+			# Save CSV file in directory and database
+			csv_file, csv_name, data_frame, recover_title = save_csv(form.csv.data, current_user.id)
 			query = Query(name=csv_name, title=csv_file, recover_title=recover_title, user_id=current_user.id)
 			db.session.add(query)
 			db.session.commit()
-			data_handler = handler('''user_path=user_path,''' data_frame=data_frame, file_name=csv_file, ml_type='classification', target='room_type')
+			data_handler = handler(data_frame=data_frame, file_name=csv_file, ml_type='classification', target='room_type')
 			data_handler.save_data()
 			flash('Your query has been submitted', 'success')
 			return redirect(url_for('results'))
 
 	return render_template('query.html', title='Query', form=form)
 
+# Define results page - requires login
 @app.route("/results", methods=['GET', 'POST'])
 @login_required
 def results():
@@ -117,12 +156,17 @@ def results():
 
 	return render_template('results.html', title='Results', posts=posts)
 
+# Save profile picture
 def save_picture(form_picture):
+	# Get a random hex 
     random_hex = secrets.token_hex(8)
+    # Split the filename into the name and file extension
     _, f_ext = os.path.splitext(form_picture.filename)
+    # Rename the file as the random hex
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
+    # Set size of image
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
@@ -131,9 +175,13 @@ def save_picture(form_picture):
     return picture_fn
 
 def save_csv(form_csv, user_id):
+	# Get random hex
 	random_hex = secrets.token_hex(8)
 	f_name, f_ext = os.path.splitext(form_csv.filename)
+	# Rename file
 	file_fn = random_hex + f_ext
+	# Save file in a new directory
+	# Create data and results sub-directory
 	#user_path = os.path.join(app.root_path, 'static/file_system', str(user_id))
 	'''
 	if os.path.isdir(user_path) == False:
@@ -145,7 +193,8 @@ def save_csv(form_csv, user_id):
 
 	file_path = os.path.join(user_path, 'data', file_fn)
 	'''
+	# Save CSV file
 	df = pd.read_csv(form_csv)
-	df.to_csv(file_path, encoding='utf8')
+	#df.to_csv(file_path, encoding='utf8')
 
-	return file_fn, f_name ''', user_path''' , df, random_hex
+	return file_fn, f_name, df, random_hex
